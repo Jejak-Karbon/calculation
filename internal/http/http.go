a3afb9b1b93c1a525d8d3b166f2953e5efe31233
siffiyan1 <siffiyanassauri1@gmail.com>
2022-07-05 17:52:02 +0700

    add feature read user carbon producer

--- internal/http/http.go
+++ internal/http/http.go
@@ -3,6 +3,7 @@ package http
 import (
 	"github.com/born2ngopi/alterra/basic-echo-mvc/internal/app/category_carbon_producer"
 	"github.com/born2ngopi/alterra/basic-echo-mvc/internal/app/carbon_producer"
+	"github.com/born2ngopi/alterra/basic-echo-mvc/internal/app/user_carbon_producer"
 	"github.com/born2ngopi/alterra/basic-echo-mvc/internal/factory"
 	"github.com/labstack/echo/v4"
 )
@@ -10,4 +11,6 @@ import (
 func NewHttp(e *echo.Echo, f *factory.Factory) {
 	category_carbon_producer.NewHandler(f).Route(e.Group("/categories_carbon_producer"))
 	carbon_producer.NewHandler(f).Route(e.Group("/carbon_producer"))
+	user_carbon_producer.NewHandler(f).Route(e.Group("/my_carbon_producer"))
 }
+
--- internal/repository/user_carbon_producer.go
+++ internal/repository/user_carbon_producer.go
@@ -1,15 +1,16 @@
 package repository
 
 import (
-	_ "strings"
+	"strings"
 	"context"
 
-	_ "github.com/born2ngopi/alterra/basic-echo-mvc/internal/dto"
+	"github.com/born2ngopi/alterra/basic-echo-mvc/internal/dto"
 	"github.com/born2ngopi/alterra/basic-echo-mvc/internal/model"
 	"gorm.io/gorm"
 )
 
 type UserCarbonProducer interface {
+	Find(ctx context.Context, payload *dto.SearchGetRequest, id uint, paginate *dto.Pagination) ([]model.UserCarbonProducer, *dto.PaginationInfo, error)
 	Create(ctx context.Context, data model.UserCarbonProducer) error
 }
 
@@ -21,6 +22,29 @@ func NewUserCarbonProducer(db *gorm.DB) *user_carbon_producer {
 	return &user_carbon_producer{
 		db,
 	}
+}
+
+func (u *user_carbon_producer) Find(ctx context.Context, payload *dto.SearchGetRequest, id uint, paginate *dto.Pagination) ([]model.UserCarbonProducer, *dto.PaginationInfo, error) {
+	var user_carbon_producer []model.UserCarbonProducer
+	var count int64
+
+	query := u.Db.WithContext(ctx).Model(&model.UserCarbonProducer{}).Where("user_id = ?  ", id)
+
+	if payload.Search != "" {
+		search := "%" + strings.ToLower(payload.Search) + "%"
+		query = query.Where("lower(name) LIKE ?  ", search)
+	}
+
+	countQuery := query
+	if err := countQuery.Count(&count).Error; err != nil {
+		return nil, nil, err
+	}
+
+	limit, offset := dto.GetLimitOffset(paginate)
+
+	err := query.Limit(limit).Offset(offset).Find(&user_carbon_producer).Error
+
+	return user_carbon_producer, dto.CheckInfoPagination(paginate, count), err
 }
 
 func (u *user_carbon_producer) Create(ctx context.Context, data model.UserCarbonProducer) error {
